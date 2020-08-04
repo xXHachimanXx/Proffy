9464c045018b663d9bfa9032f2a1275cbc81483d
xXHachimanXx <globaldep90@gmail.com>
2020-08-03 21:01:17 -0300

    Alinhando elementos da Landing.

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -6,6 +6,8 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import './styles.css'
+
 function Landing() {
   return (
     <div id="page-landing">
@@ -23,20 +25,19 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <a href="/" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
           </a>
 
-          <a href="" className="give-classes">
+          <a href="/" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas" />
             Dar aulas
           </a>
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
-
+          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
 
       </div>
