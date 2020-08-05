ff092c650294fdd5c9c8b61b797e6944ec9de6ca
xXHachimanXx <globaldep90@gmail.com>
2020-08-05 17:27:13 -0300

    Finalizando listagem das aulas.

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,5 +1,46 @@
+import { Request, Response } from 'express';
+
+import db from '../database/connection';
+import convertHourToMinutes from '../utils/convertHourToMinutes';
+interface ScheduleItem {
+    week_day: string;
+    from: string;
+    to: string;
+}
 export default class ClassesController {
-    async create(req, res) {
+
+    async index(req: Request, res: Response) {
+        const filters = req.query;
+
+        const week_day = filters.week_day as string;
+        const subject = filters.subject as string;
+        const time = filters.time as string;
+
+        if(!filters.week_day || !filters.subject || !filters.time) {
+            res.status(400).json({
+                error: "Missing filters to search classes."
+            });
+        }
+
+        const timeInMinutes = convertHourToMinutes(time);
+
+        const classes = await db('classes')
+            .whereExists(function() {
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`') // -> mais recomendado que o where comum nessa situação
+                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??', [Number(timeInMinutes)])
+                    .whereRaw('`class_schedule`.`to` > ??', [Number(timeInMinutes)])
+            })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
+
+        return res.json(classes);
+    }
+
+    async create(req: Request, res: Response) {
         const {
             name,
             avatar,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,11 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
 
-interface ScheduleItem {
-    week_day: string;
-    from: string;
-    to: string;
-}
 
-routes.post('/classes', );
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
 
 export default routes;
