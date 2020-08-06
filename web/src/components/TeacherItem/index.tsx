4a66fc6134c9c99e209344aec921abad3257c55b
xXHachimanXx <globaldep90@gmail.com>
2020-08-06 20:06:48 -0300

    Adicionando conexão com WhatsApp.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -19,6 +20,13 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -41,10 +49,14 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
             <strong>${teacher.cost}</strong>
           </p>
 
-          <button type="button">
+          <a
+            target="_blank" 
+            onClick={createNewConnection}
+            href={`https://wa.me/${teacher.whatsapp}`} 
+          >
             <img src={whatsappIcon} alt="Whatsapp" />
               Entrar em contato
-            </button>
+          </a>
         </footer>
       </p>
 
